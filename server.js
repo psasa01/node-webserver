e8cf509e06b29913bb0d6338fbb3a4a69edbe5cd
psasa01 <psasa01@gmail.com>
2017-10-17 09:49:59 +0200

    171017

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+
+/* app.use((req, res, next) => {
+    res.render('maintanence.hbs', {
+        pageContent: 'Page is under construction!'
+    });
+}); */
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -28,6 +50,7 @@ app.get('/about', (req, res) => {
         pageTitle: 'About Page - skrozof'
     });
 });
+
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
